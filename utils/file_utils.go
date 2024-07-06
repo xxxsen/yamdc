fb6b5cfb5e190be2b334a7e229363eb445a590d3
xxxsen <naivesen@gmail.com>
2024-07-06 21:43:42 +0800

    fix: media file extname

--- utils/file_utils.go
+++ utils/file_utils.go
@@ -11,7 +11,7 @@ import (
 func IsVideoFile(f string) bool {
 	ext := strings.ToLower(filepath.Ext(f))
 	switch ext {
-	case ".mp4", ".wav", ".avi", ".mkv", ".rmvb", ".m4a", ".ts":
+	case ".mp4", ".wmv", ".avi", ".mkv", ".rmvb", ".m4a", ".ts":
 		return true
 	default:
 		return false
