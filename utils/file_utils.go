76c673693fd8793bc6ecc243db88bfca42c0618c
xxxsen <naivesen@gmail.com>
2024-07-09 21:01:35 +0800

    chore: remove unused code

--- utils/file_utils.go
+++ utils/file_utils.go
@@ -8,16 +8,6 @@ import (
 	"strings"
 )
 
-func IsVideoFile(f string) bool {
-	ext := strings.ToLower(filepath.Ext(f))
-	switch ext {
-	case ".mp4", ".wmv", ".flv", ".mpeg", ".m2ts", ".mts", ".mpe", ".mpg", ".m4v", ".avi", ".mkv", ".rmvb", ".ts", ".mov", ".rm":
-		return true
-	default:
-		return false
-	}
-}
-
 func GetExtName(f string, def string) string {
 	if v := filepath.Ext(f); v != "" {
 		return v
