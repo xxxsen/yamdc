07cf3e27094f64f92c9dc83f2f9e2b7e9cc5876c
xxxsen <naivesen@gmail.com>
2024-08-20 22:27:12 +0800

    feat: add scrape source:tktube

--- searcher/plugin/field_parser.go
+++ searcher/plugin/field_parser.go
@@ -2,12 +2,28 @@ package plugin
 
 import (
 	"context"
+	"strconv"
+	"strings"
 	"yamdc/searcher/decoder"
 	"yamdc/searcher/utils"
 
 	"github.com/xxxsen/common/logutil"
 	"go.uber.org/zap"
 )
+
+func DefaultHHMMSSDurationParser(ctx context.Context) decoder.NumberParseFunc {
+	return func(v string) int64 {
+		res := strings.Split(v, ":")
+		if len(res) != 3 {
+			logutil.GetLogger(ctx).Error("invalid time format", zap.String("data", v))
+			return 0
+		}
+		h, _ := strconv.ParseInt(strings.TrimSpace(res[0]), 10, 64)
+		m, _ := strconv.ParseInt(strings.TrimSpace(res[1]), 10, 64)
+		s, _ := strconv.ParseInt(strings.TrimSpace(res[2]), 10, 64)
+		return h*3600 + m*60 + s
+	}
+}
 
 func DefaultDurationParser(ctx context.Context) decoder.NumberParseFunc {
 	return func(v string) int64 {
---
+++ searcher/plugin/tktube.go
@@ -0,0 +1,81 @@
+package plugin
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"yamdc/model"
+	"yamdc/number"
+	"yamdc/searcher/decoder"
+)
+
+type tktube struct {
+	DefaultPlugin
+}
+
+func (p *tktube) OnMakeHTTPRequest(ctx *PluginContext, n *number.Number) (*http.Request, error) {
+	nid := strings.ReplaceAll(n.GetNumberID(), "-", "--")
+	ctx.SetKey("number", n.GetNumberID())
+	uri := fmt.Sprintf("https://tktube.com/zh/search/%s/", nid)
+	return http.NewRequest(http.MethodGet, uri, nil)
+}
+
+func (p *tktube) OnHandleHTTPRequest(ctx *PluginContext, invoker HTTPInvoker, req *http.Request) (*http.Response, error) {
+	numberId := strings.ToUpper(ctx.GetKeyOrDefault("number", "").(string))
+	return HandleXPathTwoStepSearch(ctx, invoker, req, &XPathTwoStepContext{
+		Ps: []*XPathPair{
+			{
+				Name:  "links",
+				XPath: `//div[@id="list_videos_videos_list_search_result_items"]/div/a/@href`,
+			},
+			{
+				Name:  "names",
+				XPath: `//div[@id="list_videos_videos_list_search_result_items"]/div/a/strong[@class="title"]/text()`,
+			},
+		},
+		LinkSelector: func(ps []*XPathPair) (string, bool, error) {
+			links := ps[0].Result
+			names := ps[1].Result
+			for i := 0; i < len(links); i++ {
+				if strings.Contains(strings.ToUpper(names[i]), numberId) {
+					return links[i], true, nil
+				}
+			}
+			return "", false, nil
+		},
+		ValidStatusCode:       []int{http.StatusOK},
+		CheckResultCountMatch: true,
+		LinkPrefix:            "",
+	})
+}
+
+func (p *tktube) OnDecodeHTTPData(ctx *PluginContext, data []byte) (*model.AvMeta, bool, error) {
+	dec := decoder.XPathHtmlDecoder{
+		TitleExpr:           `//div[@class="headline"]/h1/text()`,
+		PlotExpr:            "",
+		ActorListExpr:       `//div[contains(text(), "女優:")]/a[contains(@href, "models")]/text()`,
+		ReleaseDateExpr:     `//div[@class="item"]/span[contains(text(), "加入日期:")]/em/text()`,
+		DurationExpr:        `//div[@class="item"]/span[contains(text(), "時長:")]/em/text()`,
+		StudioExpr:          "",
+		LabelExpr:           "",
+		DirectorExpr:        "",
+		SeriesExpr:          "",
+		GenreListExpr:       `//div[contains(text(), "標籤:")]/a[contains(@href, "tags")]/text()`,
+		CoverExpr:           `//meta[@property="og:image"]/@content`,
+		PosterExpr:          "",
+		SampleImageListExpr: "",
+	}
+	meta, err := dec.DecodeHTML(data,
+		decoder.WithDurationParser(DefaultHHMMSSDurationParser(ctx.GetContext())),
+		decoder.WithReleaseDateParser(DefaultReleaseDateParser(ctx.GetContext())),
+	)
+	if err != nil {
+		return nil, false, err
+	}
+	meta.Number = ctx.GetKeyOrDefault("number", "").(string)
+	return meta, true, nil
+}
+
+func init() {
+	Register(SSTKTube, PluginToCreator(&tktube{}))
+}
