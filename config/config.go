0d2f8107e9662d5bb9b84d54c78bf5346b5dadad
xxxsen <naivesen@gmail.com>
2024-08-27 20:27:29 +0800

    chore: add to default list

--- config/config.go
+++ config/config.go
@@ -41,13 +41,14 @@ func defaultConfig() *Config {
 			"jav321",
 			"caribpr",
 			"18av",
+			"njav",
 			"freejavbt",
 			"tktube",
 			"avsox",
 		},
 		CategoryPlugins: []CategoryPlugin{
 			//如果存在分配配置, 那么当番号被识别为特定分类的场景下, 将会使用分类插件直接查询
-			{Name: "FC2", Plugins: []string{"fc2", "18av", "freejavbt", "tktube", "avsox"}},
+			{Name: "FC2", Plugins: []string{"fc2", "18av", "njav", "freejavbt", "tktube", "avsox"}},
 		},
 		Handlers: []string{
 			"image_transcoder",
