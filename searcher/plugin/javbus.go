c00b6a368979b7f0e95fc6ce8e157b3d712f4e5b
xxxsen <naivesen@gmail.com>
2024-10-05 23:00:13 +0800

    fix: title

--- searcher/plugin/javbus.go
+++ searcher/plugin/javbus.go
@@ -40,7 +40,7 @@ func (p *javbus) OnDecorateRequest(ctx *PluginContext, req *http.Request) error 
 func (p *javbus) OnDecodeHTTPData(ctx *PluginContext, data []byte) (*model.AvMeta, bool, error) {
 	dec := decoder.XPathHtmlDecoder{
 		NumberExpr:          `//div[@class="row movie"]/div[@class="col-md-3 info"]/p[span[contains(text(),'識別碼:')]]/span[2]/text()`,
-		TitleExpr:           `/html/head/title`,
+		TitleExpr:           `//div[@class="container"]/h3`,
 		ActorListExpr:       `//div[@class="star-name"]/a/text()`,
 		ReleaseDateExpr:     `//div[@class="row movie"]/div[@class="col-md-3 info"]/p[span[contains(text(),'發行日期:')]]/text()[1]`,
 		DurationExpr:        `//div[@class="row movie"]/div[@class="col-md-3 info"]/p[span[contains(text(),'長度:')]]/text()[1]`,
@@ -50,6 +50,7 @@ func (p *javbus) OnDecodeHTTPData(ctx *PluginContext, data []byte) (*model.AvMet
 		GenreListExpr:       `//div[@class="row movie"]/div[@class="col-md-3 info"]/p/span[@class="genre"]/label[input[@name="gr_sel"]]/a/text()`,
 		CoverExpr:           `//div[@class="row movie"]/div[@class="col-md-9 screencap"]/a[@class="bigImage"]/@href`,
 		PosterExpr:          "",
+		PlotExpr:            `//meta[@name="description"]/@content`,
 		SampleImageListExpr: `//div[@id="sample-waterfall"]/a[@class="sample-box"]/@href`,
 	}
 	rs, err := dec.DecodeHTML(data,
