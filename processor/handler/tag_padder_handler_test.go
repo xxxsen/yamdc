f4a9ebb14cecac031678349161e6e7e92ece4e12
xxxsen <naivesen@gmail.com>
2024-11-02 19:33:20 +0800

    feat: allow use number prefix as tag

--- processor/handler/tag_padder_handler_test.go
+++ processor/handler/tag_padder_handler_test.go
@@ -9,14 +9,47 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testPair struct {
+	in        string
+	tagCount  int
+	existTags []string
+}
+
 func TestTagPadde(t *testing.T) {
-	num, err := number.Parse("fc2-1234-c-4k")
-	assert.NoError(t, err)
-	padder := &tagPadderHandler{}
-	fc := &model.FileContext{
-		Number: num,
-		Meta:   &model.AvMeta{},
+	tsts := []testPair{
+		{
+			in:        "fc2-1234-c-4k",
+			tagCount:  4,
+			existTags: []string{},
+		},
+		{
+			in:        "fc2-ppv-123",
+			tagCount:  2,
+			existTags: []string{"FC2"},
+		},
+		{
+			in:        "heyzo-123",
+			tagCount:  2,
+			existTags: []string{"HEYZO"},
+		},
+		{
+			in:        "111111-11",
+			tagCount:  1,
+			existTags: []string{},
+		},
 	}
-	padder.Handle(context.Background(), fc)
-	assert.Equal(t, 3, len(fc.Meta.Genres))
+	for _, item := range tsts {
+		num, err := number.Parse(item.in)
+		assert.NoError(t, err)
+		padder := &tagPadderHandler{}
+		fc := &model.FileContext{
+			Number: num,
+			Meta:   &model.AvMeta{},
+		}
+		padder.Handle(context.Background(), fc)
+		assert.Equal(t, item.tagCount, len(fc.Meta.Genres))
+		for _, existTag := range item.existTags {
+			assert.Contains(t, fc.Meta.Genres, existTag)
+		}
+	}
 }
