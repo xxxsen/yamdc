4ff7dbd7b27170824fe2125ba77f460b05b47776
xxxsen <naivesen@gmail.com>
2025-01-28 20:54:41 +0800

    feat: auto delete expired data

--- store/sqlite_storage.go
+++ store/sqlite_storage.go
@@ -11,19 +11,32 @@ import (
 	_ "github.com/glebarez/go-sqlite"
 )
 
+const (
+	defaultExpireTime = 90 * 24 * time.Hour //默认存储3个月, 超过就删了吧, 实在没想到有啥东西需要永久存储的?
+)
+
 type sqliteStore struct {
 	db *sql.DB
 }
 
-func (s *sqliteStore) init() error {
-	createTable := `CREATE TABLE IF NOT EXISTS cache_tab (
-        key TEXT PRIMARY KEY,
-        value BLOB,
-        expire_at INTEGER
-    );`
-	_, err := s.db.Exec(createTable)
-	if err != nil {
-		return err
+func (s *sqliteStore) init(ctx context.Context) error {
+	execList := []struct {
+		sql  string
+		args []interface{}
+	}{
+		{`CREATE TABLE IF NOT EXISTS cache_tab ( 
+			key TEXT PRIMARY KEY, 
+			value BLOB, 
+			expire_at INTEGER 
+		);`, nil},
+		{"CREATE INDEX if not exists idx_expireat on cache_tab(expire_at);", nil},
+		{"DELETE from cache_tab where expire_at <= ?", []interface{}{time.Now().Unix()}},
+	}
+	for _, item := range execList {
+		_, err := s.db.ExecContext(ctx, item.sql, item.args...)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -31,7 +44,7 @@ func (s *sqliteStore) init() error {
 func (s *sqliteStore) GetData(ctx context.Context, key string) ([]byte, error) {
 	var val []byte
 	now := time.Now().Unix()
-	err := s.db.QueryRow("SELECT value FROM cache_tab WHERE key = ? and (expire_at > ? or expire_at = 0)", key, now).Scan(&val)
+	err := s.db.QueryRowContext(ctx, "SELECT value FROM cache_tab WHERE key = ? and expire_at > ?", key, now).Scan(&val)
 	if err != nil {
 		return nil, err
 	}
@@ -40,10 +53,13 @@ func (s *sqliteStore) GetData(ctx context.Context, key string) ([]byte, error) {
 
 func (s *sqliteStore) PutData(ctx context.Context, key string, value []byte, expire time.Duration) error {
 	var expireAt int64 = 0
+	if expire == 0 {
+		expire = defaultExpireTime //use default expire time
+	}
 	if expire > 0 {
 		expireAt = time.Now().Add(expire).Unix()
 	}
-	_, err := s.db.Exec("INSERT OR REPLACE INTO cache_tab (key, value, expire_at) VALUES (?, ?, ?)", key, value, expireAt)
+	_, err := s.db.ExecContext(ctx, "INSERT OR REPLACE INTO cache_tab (key, value, expire_at) VALUES (?, ?, ?)", key, value, expireAt)
 	if err != nil {
 		return err
 	}
@@ -53,7 +69,7 @@ func (s *sqliteStore) PutData(ctx context.Context, key string, value []byte, exp
 func (s *sqliteStore) IsDataExist(ctx context.Context, key string) (bool, error) {
 	var cnt int64
 	now := time.Now().Unix()
-	err := s.db.QueryRow("SELECT count(*) FROM cache_tab WHERE key = ? and (expire_at > ? or expire_at = 0)", key, now).Scan(&cnt)
+	err := s.db.QueryRowContext(ctx, "SELECT count(*) FROM cache_tab WHERE key = ? and expire_at > ?", key, now).Scan(&cnt)
 	if errors.Is(err, sql.ErrNoRows) {
 		return false, nil
 	}
@@ -76,7 +92,7 @@ func NewSqliteStorage(path string) (IStorage, error) {
 		return nil, err
 	}
 	s := &sqliteStore{db: db}
-	if err := s.init(); err != nil {
+	if err := s.init(context.Background()); err != nil {
 		return nil, err
 	}
 	return s, nil
