3a67ecc9e65112b7170383edd8af709bf1182b5f
xxxsen <naivesen@gmail.com>
2025-10-04 19:54:05 +0800

    refactor: migrate to toml config

--- config/rule_config.go
+++ config/rule_config.go
@@ -3,14 +3,14 @@ package config
 var sysRuleConfig = RuleConfig{
 	NumberRewriter: LinkConfig{
 		Type: "network",
-		Link: "https://raw.githubusercontent.com/xxxsen/yamdc-script/refs/heads/master/number_rewriter.yaml",
+		Link: "https://raw.githubusercontent.com/xxxsen/yamdc-script/refs/heads/master/scripts/number_rewriter.toml",
 	},
 	NumberCategorier: LinkConfig{
 		Type: "network",
-		Link: "https://raw.githubusercontent.com/xxxsen/yamdc-script/refs/heads/master/number_categorier.yaml",
+		Link: "https://raw.githubusercontent.com/xxxsen/yamdc-script/refs/heads/master/scripts/number_categorier.toml",
 	},
 	NumberUncensorTester: LinkConfig{
 		Type: "network",
-		Link: "https://raw.githubusercontent.com/xxxsen/yamdc-script/refs/heads/master/number_uncensor_tester.yaml",
+		Link: "https://raw.githubusercontent.com/xxxsen/yamdc-script/refs/heads/master/scripts/number_uncensor_tester.toml",
 	},
 }
